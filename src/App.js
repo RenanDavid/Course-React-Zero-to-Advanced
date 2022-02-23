187603c02d0145b78ff57c30dddc2938cb23f6a9
RenanDavid <pro.alfayank@gmail.com>
2022-02-23 17:34:16 -0300

    importando primeiro componente como teste para o app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Comentario from './components/Comentario';
 
 function App() {
   return (
     <div className="App">
       <h1>Online Notepad</h1>
+      <Comentario/>
     </div>
   );
 }
