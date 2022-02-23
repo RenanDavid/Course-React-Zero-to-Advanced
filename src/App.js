43c368f0af1838de57fd09752d5323628791ebb9
RenanDavid <pro.alfayank@gmail.com>
2022-02-23 19:00:39 -0300

    Colocando propriedades no Componente Comentario

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import Comentario from './components/Comentario';
 function App() {
   return (
     <div className="App">
-      <h1>Online Notepad</h1>
-      <Comentario/>
+      <h1>Notepad</h1>
+      <Comentario nome="Renan David" email="renandavid6@gmail.com" data={new Date(2022,2,23)}>
+        Olá Notepad!
+      </Comentario>
+
+      <Comentario nome="Renan David" email="renandavid6@gmail.com" data={new Date(2022,2,23)}>
+        Anotação número 1!
+      </Comentario>
     </div>
   );
 }
